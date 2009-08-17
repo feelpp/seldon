4fae8d48cbfb75e813b88ad216919ba5f98c5180
Vivien Mallet <Vivien.Mallet@ecl2002.ec-lyon.fr>
2009-08-17 18:03:09 +0200

    Improved the compliance with HTML standards.

--- doc/guide/header.php
+++ doc/guide/header.php
@@ -1,16 +1,14 @@
-<?php echo '<?xml version="1.0"  encoding="iso-8859-1"?'.'>' ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
 
 <?php $root='..';?>
 
 <head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <title>Seldon user's guide</title>
-<link rel="stylesheet" type="text/css" href="<?php echo $root?>/content.css"/>
-<link rel="stylesheet" href="tabs.css" type="text/css"/>
-<link rel="stylesheet" href="guide.css" type="text/css"/>
+<link rel="stylesheet" type="text/css" href="<?php echo $root?>/content.css">
+<link rel="stylesheet" href="tabs.css" type="text/css">
+<link rel="stylesheet" href="guide.css" type="text/css">
 <script type="text/javascript" src="<?php echo $root.'/';?>prettify.js"></script>
 </head>
 
@@ -115,7 +113,7 @@ or basename($_SERVER['REQUEST_URI'], ".php") == "iterative")
 </li> </ul> </li>
 <li class="jelly"> <?php HL($file, "namespacemembers", "Functions");?> </li>
 <li class="jelly"> Search for <form action="search.php" method="get">
-    <input class="search" type="text" name="query" value="" size="20" accesskey="s"/>
+    <input class="search" type="text" name="query" value="" size="20" accesskey="s">
   </form>
 </li>
 <!-- <li class="jelly"> <?php HL($file, "faq", "F.A.Q.");?> </li>-->
