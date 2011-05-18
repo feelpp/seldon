e40bf8242d690bbaf5f491495ca8b73808ff556d
Marc Fragu <marc.fragu@inria.fr>
2011-05-18 15:04:21 +0200

    Implemented function 'SetCol(const Vector<VectFull>, int, Matrix<RowSparse>)'.

--- matrix/Functions.cxx
+++ matrix/Functions.cxx
@@ -205,6 +205,23 @@ namespace Seldon
   }
 
 
+  template <class T0, class Allocator0,
+	    class T1, class Allocator1>
+  void SetCol(const Vector<T1, VectFull, Allocator1>& X,
+	      int j, Matrix<T0, General, RowSparse, Allocator0>& M)
+  {
+    Vector<T1, VectSparse, Allocator1> X_sparse;
+    for (int k = 0; k < X.GetLength(); k++)
+      {
+        T1 value = X(k);
+        if (value != T1(0.))
+          X_sparse.AddInteraction(k, value);
+      }
+
+    SetCol(X_sparse, j, M);
+  }
+
+
   template <class T0, class Allocator0, class T1, class Allocator1>
   void SetCol(const Vector<T1, VectSparse, Allocator1>& X,
 	      int j, Matrix<T0, General, RowSparse, Allocator0>& M)
